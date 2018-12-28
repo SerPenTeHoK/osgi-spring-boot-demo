4f9668860443b8ef6402e2ebc5e3144d9c3eb35d
Dmitriy Andrievskiy <DKAndrievsky@luxoft.com>
2018-12-28 13:17:02 +0300

    scanning works

--- src/main/java/by/kolodyuk/osgi/springboot/SpringBootBundleActivator.java
+++ src/main/java/by/kolodyuk/osgi/springboot/SpringBootBundleActivator.java
@@ -7,22 +7,52 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Import(AppConfig.class)
-@SpringBootConfiguration
+import org.springframework.osgi.io.OsgiBundleResourceLoader;
+import org.springframework.osgi.io.OsgiBundleResourcePatternResolver;
+//import org.eclipse.core.runtime.FileLocator; 
+
+
+/*
 @EnableAutoConfiguration
+@SpringBootApplication
+@ImportResource("classpath:spring-config.xml")
+*/
+//@Configuration
+//@ComponentScan("classpath*:by.kolodyuk.osgi.springboot.controller")
+//@EnableAutoConfiguration
+@SpringBootApplication
 public class SpringBootBundleActivator implements BundleActivator {
 
     ConfigurableApplicationContext appContext;
 
     @Override
     public void start(BundleContext bundleContext) {
+		System.out.println("2. Start started");
+		/*try {
+			java.net.URL url = new java.net.URL("/"); 
+			String realPath = FileLocator.toFileURL(url).getPath(); 
+			System.out.println("path: " + realPath);
+		} catch (Throwable e){
+			System.out.println("e: " + e.getMessage());
+		}*/
+		
         Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
-        appContext = SpringApplication.run(SpringBootBundleActivator.class);
+		OsgiBundleResourcePatternResolver rl = new OsgiBundleResourcePatternResolver(bundleContext.getBundle());
+        appContext = new SpringApplication(rl, SpringBootBundleActivator.class).run();
+		System.out.println("2. Start finished");
     }
 
     @Override
     public void stop(BundleContext bundleContext) {
         SpringApplication.exit(appContext, () -> 0);
     }
+	
+	public static void main(String[] args) {
+        SpringApplication.run(SpringBootBundleActivator.class);
+    }
 }
+
--- src/main/java/by/kolodyuk/osgi/springboot/controller/SimpleController.java
+++ src/main/java/by/kolodyuk/osgi/springboot/controller/SimpleController.java
@@ -2,13 +2,12 @@ package by.kolodyuk.osgi.springboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class SimpleController {
-
-    @GetMapping("/echo")
-    public String echo() {
-        return "echo";
+    @GetMapping("/simple")
+    public String index() {
+        return "some text";
     }
-
 }
